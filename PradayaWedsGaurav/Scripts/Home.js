6dcbf54eeca54c9d5a3e46f3157325bbf67e23d1
Rahul Kargwal <rahul.kargwal@elevateservices.com>
2015-08-24 22:20:52 +0530

    Audio Controls
    Link in js File

---
+++ PradayaWedsGaurav/Scripts/Home.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function ($) {
+    $('#banner-fade').bjqs({
+        height: 320,
+        width: 620,
+        responsive: true
+    });
+
+    // http://www.w3schools.com/html/html5_audio.asp
+    // http://www.w3schools.com/tags/ref_av_dom.asp
+    var vid = document.getElementById("audioHome");
+    vid.volume = 0.5;
+    vid.controls = true;
+    vid.autoplay = true;
+    vid.loop = true;
+    vid.load();
+});
